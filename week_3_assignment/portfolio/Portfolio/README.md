7e71b816a6bb0c90af27c360b1bd9b00a99bcd26
Karen Kirr <kkirr77@yahoo.com>
2014-01-26 10:33:36 -0800

    add Store app that is based on episode 327 minitest

---
+++ week_3_assignment/portfolio/Portfolio/README.md
@@ -0,0 +1,7 @@
+
+Rails Week 2
+
+A simple welcome web page integrating BDD testing:
+
+screenshot:
+![Screenshot](https://raw.github.com/kirrk/RubyWinter2014/master/portfolio/Portfolio/Portfolio.png "Screenshot of passing web page")
---
+++ week_3_assignment/store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'ef344bf4aad7d60728f581854e31812e58768182ea2a209a3bd47ee46aa7ddae671ab1ab2902fe32d6ac2c31a44391f4c62682fb9878497c2116623d968fbc91'
