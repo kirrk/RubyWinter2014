e94fabdc917cc74a7dec3acd2ec9294008b9f8d1
Karen Kirr <kkirr77@yahoo.com>
2014-02-01 13:46:32 -0800

    add zurb foundation files

---
+++ week_4_assignment/store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '4300544d4b3294ef0181ec5f15fe5f90ed826e6e0546f5d94fe3078eaa6c0041bd6ef84952b18c00525e3087c1abc6f01d8349d86a041833b5b67e3bea4abbc1'
